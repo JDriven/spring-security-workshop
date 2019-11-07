bcf559f4bf36139f5275c7769aff24729f4582bb
Tim te Beek <tim.te.beek@jdriven.com>
2019-11-07 12:22:37 +0100

    Add spring data queries workshop

---
+++ limit-spring-data-queries/src/main/java/com/jdriven/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.jdriven;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+	// TODO Integrate Spring Data with Spring Security, expand on the security expression in PreferencesRepository, and
+	// have the restrictions enforced by Spring Security
+}
