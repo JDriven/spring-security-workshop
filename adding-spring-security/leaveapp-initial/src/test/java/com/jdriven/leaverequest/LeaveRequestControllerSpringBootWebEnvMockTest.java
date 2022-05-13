5ce49c0f5b49e33d5ecafbbea83ce123c6ceeff8
Tom de Vroomen <tom.de.vroomen@jdriven.com>
2022-05-13 14:22:15 +0200

    Make the workshop more actionable - Update README with actionable items with links to the specific part in the blogpost - Rewrite links to github, not JDriven blog post - Remove dependency on WireMock to run tests

--- adding-spring-security/leaveapp-initial/src/test/java/com/jdriven/leaverequest/LeaveRequestControllerSpringBootWebEnvMockTest.java
+++ adding-spring-security/leaveapp-initial/src/test/java/com/jdriven/leaverequest/LeaveRequestControllerSpringBootWebEnvMockTest.java
@@ -5,17 +5,22 @@ import java.util.UUID;
 import com.jdriven.leaverequest.LeaveRequest;
 import com.jdriven.leaverequest.LeaveRequestRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.jdriven.leaverequest.LeaveRequest.Status.APPROVED;
 import static com.jdriven.leaverequest.LeaveRequest.Status.PENDING;
 import static java.time.LocalDate.of;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -37,87 +42,93 @@ class LeaveRequestControllerSpringBootWebEnvMockTest {
 		repository.clear();
 	}
 
-	@Test
-	void testRequest() throws Exception {
-		mockmvc.perform(post("/request/{employee}", "Alice")
-				// XXX Authenticate as Alice when making this request
-				.param("from", "2019-11-30")
-				.param("to", "2019-12-03"))
-				.andExpect(status().isAccepted())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$.employee").value("Alice"))
-				.andExpect(jsonPath("$.status").value("PENDING"));
+	@Nested
+	class AuthorizeUser {
+
+		@Test
+		void testRequest() throws Exception {
+			mockmvc.perform(post("/request/{employee}", "Alice")
+							// XXX Authenticate as Alice when making this request
+							.param("from", "2019-11-30")
+							.param("to", "2019-12-03"))
+					.andExpect(status().isAccepted())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+					.andExpect(jsonPath("$.employee").value("Alice"))
+					.andExpect(jsonPath("$.status").value("PENDING"));
+		}
+
+		@Test
+		void testViewId() throws Exception {
+			LeaveRequest saved = repository
+					.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), APPROVED));
+			mockmvc.perform(get("/view/id/{id}", saved.getId()))
+					// XXX Authenticate as Alice when making this request
+					.andExpect(status().isOk())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+					.andExpect(jsonPath("$.employee").value("Alice"))
+					.andExpect(jsonPath("$.status").value("APPROVED"));
+		}
+
+		@Test
+		void testViewEmployee() throws Exception {
+			repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), APPROVED));
+			mockmvc.perform(get("/view/employee/{employee}", "Alice"))
+					// XXX Authenticate as Alice when making this request
+					.andExpect(status().isOk())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+					.andExpect(jsonPath("$[0].employee").value("Alice"))
+					.andExpect(jsonPath("$[0].status").value("APPROVED"));
+		}
 	}
 
-	@Test
-	void testApprove() throws Exception {
-		LeaveRequest saved = repository
-				.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
-		mockmvc.perform(post("/approve/{id}", saved.getId()))
-				// XXX Authenticate with HR role when making this request
-				.andExpect(status().isAccepted())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$.employee").value("Alice"))
-				.andExpect(jsonPath("$.status").value("APPROVED"));
-	}
+	@Nested
+	class AuthorizeRole {
+		@Test
+		void testApprove() throws Exception {
+			LeaveRequest saved = repository
+					.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
+			mockmvc.perform(post("/approve/{id}", saved.getId()))
+					// XXX Authenticate with HR role when making this request
+					.andExpect(status().isAccepted())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+					.andExpect(jsonPath("$.employee").value("Alice"))
+					.andExpect(jsonPath("$.status").value("APPROVED"));
+		}
 
-	@Test
-	void testApproveMissing() throws Exception {
-		mockmvc.perform(post("/approve/{id}", UUID.randomUUID()))
-				// XXX Authenticate with HR role when making this request
-				.andExpect(status().isNoContent());
-	}
+		@Test
+		void testApproveMissing() throws Exception {
+			mockmvc.perform(post("/approve/{id}", UUID.randomUUID()))
+					// XXX Authenticate with HR role when making this request
+					.andExpect(status().isNoContent());
+		}
 
-	@Test
-	void testDeny() throws Exception {
-		LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
-		mockmvc.perform(post("/deny/{id}", saved.getId()))
-				// XXX Authenticate with HR role when making this request
-				.andExpect(status().isAccepted())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$.employee").value("Alice"))
-				.andExpect(jsonPath("$.status").value("DENIED"));
-	}
+		@Test
+		void testDeny() throws Exception {
+			LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
+			mockmvc.perform(post("/deny/{id}", saved.getId()))
+					// XXX Authenticate with HR role when making this request
+					.andExpect(status().isAccepted())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+					.andExpect(jsonPath("$.employee").value("Alice"))
+					.andExpect(jsonPath("$.status").value("DENIED"));
+		}
 
-	@Test
-	void testViewId() throws Exception {
-		LeaveRequest saved = repository
-				.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), APPROVED));
-		mockmvc.perform(get("/view/id/{id}", saved.getId()))
-				// XXX Authenticate as Alice when making this request
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$.employee").value("Alice"))
-				.andExpect(jsonPath("$.status").value("APPROVED"));
-	}
+		@Test
+		void testViewIdMissing() throws Exception {
+			mockmvc.perform(get("/view/id/{id}", UUID.randomUUID()))
+					// XXX Authenticate with HR role when making this request
+					.andExpect(status().isNoContent());
+		}
 
-	@Test
-	void testViewIdMissing() throws Exception {
-		mockmvc.perform(get("/view/id/{id}", UUID.randomUUID()))
-				// XXX Authenticate with HR role when making this request
-				.andExpect(status().isNoContent());
+		@Test
+		void testViewAll() throws Exception {
+			repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), APPROVED));
+			mockmvc.perform(get("/view/all"))
+					// XXX Authenticate with HR role when making this request
+					.andExpect(status().isOk())
+					.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+					.andExpect(jsonPath("$[0].employee").value("Alice"))
+					.andExpect(jsonPath("$[0].status").value("APPROVED"));
+		}
 	}
-
-	@Test
-	void testViewEmployee() throws Exception {
-		repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), APPROVED));
-		mockmvc.perform(get("/view/employee/{employee}", "Alice"))
-				// XXX Authenticate as Alice when making this request
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$[0].employee").value("Alice"))
-				.andExpect(jsonPath("$[0].status").value("APPROVED"));
-	}
-
-	@Test
-	void testViewAll() throws Exception {
-		repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), APPROVED));
-		mockmvc.perform(get("/view/all"))
-				// XXX Authenticate with HR role when making this request
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$[0].employee").value("Alice"))
-				.andExpect(jsonPath("$[0].status").value("APPROVED"));
-	}
-
 }
--- adding-spring-security/leaveapp-initial/src/test/java/com/jdriven/leaverequest/LeaveRequestControllerSpringBootWebEnvRandomPortTest.java
+++ adding-spring-security/leaveapp-initial/src/test/java/com/jdriven/leaverequest/LeaveRequestControllerSpringBootWebEnvRandomPortTest.java
@@ -2,25 +2,30 @@ package com.jdriven.leaverequest;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
-import com.jdriven.leaverequest.LeaveRequest;
-import com.jdriven.leaverequest.LeaveRequestDTO;
-import com.jdriven.leaverequest.LeaveRequestRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 import static com.jdriven.leaverequest.LeaveRequest.Status.APPROVED;
 import static com.jdriven.leaverequest.LeaveRequest.Status.DENIED;
 import static com.jdriven.leaverequest.LeaveRequest.Status.PENDING;
 import static java.time.LocalDate.of;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpStatus.ACCEPTED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
@@ -30,111 +35,115 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class LeaveRequestControllerSpringBootWebEnvRandomPortTest {
 
-	private static final String TOKEN_ALICE_VALID_UNTIL_AUGUST_2022 = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJRSS1mdW1sLTRyTnBrdVZxWVg0elpuZlRUZW1hSkxoZ183Z0dULTZiSFlVIn0.eyJqdGkiOiI0OWFkZmVlOS02MGEwLTQxMTItOGI1OS1hZWEwOTliYjlmYTIiLCJleHAiOjE2NTk4OTg3NjYsIm5iZiI6MCwiaWF0IjoxNTczNDk4NzY2LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwOTAvYXV0aC9yZWFsbXMvc3ByaW5nLWNsb3VkLWdhdGV3YXktcmVhbG0iLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiYWQ4MjRkZmMtNmFmZi00NWEzLWJlOWQtOTU0ZDUwZmJkYjdhIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic3ByaW5nLWNsb3VkLWdhdGV3YXktY2xpZW50Iiwibm9uY2UiOiJwMGtsejZlc0VYNnYtWU8tXzVGd2JITk95N2NudklibEsweTBiWi1iUVpNIiwiYXV0aF90aW1lIjoxNTczNDk4NzY2LCJzZXNzaW9uX3N0YXRlIjoiZjIyMjY4ZTAtOTJlZS00MDU4LTgzMzctMzkyNDg4YjJlNTQwIiwiYWNyIjoiMSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJvcGVuaWQgcGhvbmUgbWljcm9wcm9maWxlLWp3dCBvZmZsaW5lX2FjY2VzcyBwcm9maWxlIGVtYWlsIGFkZHJlc3MiLCJ1cG4iOiJhbGljZSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhZGRyZXNzIjp7fSwibmFtZSI6IkFsaWNlIiwiZ3JvdXBzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXSwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWxpY2UiLCJnaXZlbl9uYW1lIjoiQWxpY2UifQ.hHK8E-01eFb4UZtBjclVronp-6jfVbhxh9U0m0DEdYQE8YGWFCjMxUcfeI0Rx9zueeC_HJsFT4wcegs8Qh6Z2n7IGWbf0Vtg0fzVH9vFXkwbFBdbCZJz4MZ1Zc3cpC2UrSnuPDc8EIn1FQrKIEF8nqEcNzcL3ujxligvOern2A4xUS1GLqrLKmbPPbKocESZb75EMbmEPFDoctgI1n4vniOz9j242WxbcMjM7INeIsPCKWqvgaeYHOKlgW2CfHfsLPr_XQw5VvCMWJ1GHficCwwjyL-PeemHmBS6eO6EnHpl5Ftm7siATcZuJXQHTosb6b38Gkw2s6-yGQJcbQc01g";
-	private static final String TOKEN_HR_VALID_UNTIL_AUGUST_2022 = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJRSS1mdW1sLTRyTnBrdVZxWVg0elpuZlRUZW1hSkxoZ183Z0dULTZiSFlVIn0.eyJqdGkiOiJhZjJkYjUzZi0zMzA3LTRiYmItOWIyMy01NjI4NzYxOTNiOTAiLCJleHAiOjE2NTk4OTgyMjIsIm5iZiI6MCwiaWF0IjoxNTczNDk4MjIyLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwOTAvYXV0aC9yZWFsbXMvc3ByaW5nLWNsb3VkLWdhdGV3YXktcmVhbG0iLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiYjc2ODI2NjQtZWJhZC00NzAzLTgyNTctNDIyY2QwNTJhNjU4IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic3ByaW5nLWNsb3VkLWdhdGV3YXktY2xpZW50Iiwibm9uY2UiOiJDMFktek5kTUt6UWwweGdESDc1OWZJOU13ek5iY3ROT3I2M0ZOMUFSX1NzIiwiYXV0aF90aW1lIjoxNTczNDk4MjIyLCJzZXNzaW9uX3N0YXRlIjoiODA5NGMxZGUtMTNmYS00MzQzLWEyN2MtODRhZTE3NDJlMDA5IiwiYWNyIjoiMSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsIkhSIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwaG9uZSBtaWNyb3Byb2ZpbGUtand0IG9mZmxpbmVfYWNjZXNzIHByb2ZpbGUgZW1haWwgYWRkcmVzcyIsInVwbiI6ImJvYiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhZGRyZXNzIjp7fSwibmFtZSI6IkJvYiIsImdyb3VwcyI6WyJvZmZsaW5lX2FjY2VzcyIsIkhSIiwidW1hX2F1dGhvcml6YXRpb24iXSwicHJlZmVycmVkX3VzZXJuYW1lIjoiYm9iIiwiZ2l2ZW5fbmFtZSI6IkJvYiJ9.DxqIEqanm_7pM08amW6VQ7EBXta54c7kV2ozewYZ_K6_nqOZHAQMWiH-DTnCLlNPnoKZeoLsf_7QcRAXijGoCiRYy0suhGvLAAMCtQgDMfmgMYCZV-sOkHNX8AZDzDj3tXQs1zVQOc3zw9M3Byt_e-0KzxcIi4EUcD8p5fi7NTsynV7Lm6KQVMQTVE-eWBN9wtNo6Lacv4WeFcOTYTRTVRHJU72rrXe6hSDpRd90__T2PsncnqvKPhtaSe4klgaENflxQRTEVKu_oDah4MabwFW1yqQRX68OvUdmtOI2sd05p1juYwuIbSThGtcgrqA1VomgHu0fbp6bgvd1Q_neuw";
+	private static final ParameterizedTypeReference<List<LeaveRequestDTO>> TYPE_REFERENCE = new ParameterizedTypeReference<>() {};
 
 	@Autowired
 	private LeaveRequestRepository repository;
 
 	@Autowired
-	private TestRestTemplate resttemplate;
+	private TestRestTemplate restTemplate;
 
 	@BeforeEach
 	void beforeEach() {
 		repository.clear();
 	}
 
-	@Test
-	void testRequest() throws Exception {
-		LocalDate from = of(2019, 11, 30);
-		LocalDate to = of(2019, 12, 3);
-		// XXX Authenticate as Alice when making this request
-		ResponseEntity<LeaveRequestDTO> response = resttemplate.postForEntity("/request/{employee}?from={from}&to={to}",
-				null, LeaveRequestDTO.class, "Alice", from, to);
-		assertThat(response.getStatusCode()).isEqualByComparingTo(ACCEPTED);
-		assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
-		assertThat(response.getBody().getEmployee()).isEqualTo("Alice");
-		assertThat(response.getBody().getFromDate()).isEqualTo(from);
-		assertThat(response.getBody().getToDate()).isEqualTo(to);
-		assertThat(response.getBody().getStatus()).isEqualByComparingTo(PENDING);
+	@Nested
+	class AuthorizeUser {
+
+		@Test
+		void testRequest() {
+			LocalDate from = of(2019, 11, 30);
+			LocalDate to = of(2019, 12, 3);
+			// XXX Authenticate as Alice when making this request
+			ResponseEntity<LeaveRequestDTO> response = restTemplate.postForEntity("/request/{employee}?from={from}&to={to}",
+					null, LeaveRequestDTO.class, "Alice", from, to);
+			assertThat(response.getStatusCode()).isEqualByComparingTo(ACCEPTED);
+			assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
+			assertThat(response.getBody().getEmployee()).isEqualTo("Alice");
+			assertThat(response.getBody().getFromDate()).isEqualTo(from);
+			assertThat(response.getBody().getToDate()).isEqualTo(to);
+			assertThat(response.getBody().getStatus()).isEqualByComparingTo(PENDING);
+		}
+
+		@Test
+		void testViewId() {
+			LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
+			// XXX Authenticate as Alice when making this request
+			ResponseEntity<LeaveRequestDTO> response = restTemplate.postForEntity("/approve/{id}", null,
+					LeaveRequestDTO.class, saved.getId());
+			assertThat(response.getStatusCode()).isEqualByComparingTo(OK);
+			assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
+			assertThat(response.getBody().getEmployee()).isEqualTo("Alice");
+			assertThat(response.getBody().getStatus()).isEqualByComparingTo(PENDING);
+		}
+
+		@Test
+		void testViewEmployee() {
+			repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
+			// XXX Authenticate as Alice when making this request
+			ResponseEntity<List<LeaveRequestDTO>> response = restTemplate.exchange("/view/employee/{employee}", GET, null,
+					TYPE_REFERENCE, "Alice");
+			assertThat(response.getStatusCode()).isEqualByComparingTo(OK);
+			assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
+			assertThat(response.getBody().get(0).getEmployee()).isEqualTo("Alice");
+			assertThat(response.getBody().get(0).getStatus()).isEqualByComparingTo(PENDING);
+		}
 	}
 
-	@Test
-	void testApprove() throws Exception {
-		LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
-		// XXX Authenticate with HR role when making this request
-		ResponseEntity<LeaveRequestDTO> response = resttemplate.postForEntity("/approve/{id}", null,
-				LeaveRequestDTO.class, saved.getId());
-		assertThat(response.getStatusCode()).isEqualByComparingTo(ACCEPTED);
-		assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
-		assertThat(response.getBody().getEmployee()).isEqualTo("Alice");
-		assertThat(response.getBody().getStatus()).isEqualByComparingTo(APPROVED);
-	}
+	@Nested
+	class AuthorizeRole {
 
-	@Test
-	void testApproveMissing() throws Exception {
-		// XXX Authenticate with HR role when making this request
-		ResponseEntity<LeaveRequestDTO> response = resttemplate.postForEntity("/approve/{id}", null,
-				LeaveRequestDTO.class, UUID.randomUUID());
-		assertThat(response.getStatusCode()).isEqualByComparingTo(NO_CONTENT);
-	}
+		@Test
+		void testApprove() {
+			LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
+			// XXX Authenticate with HR role when making this request
+			ResponseEntity<LeaveRequestDTO> response = restTemplate.postForEntity("/approve/{id}", null,
+					LeaveRequestDTO.class, saved.getId());
+			assertThat(response.getStatusCode()).isEqualByComparingTo(ACCEPTED);
+			assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
+			assertThat(response.getBody().getEmployee()).isEqualTo("Alice");
+			assertThat(response.getBody().getStatus()).isEqualByComparingTo(APPROVED);
+		}
 
-	@Test
-	void testDeny() throws Exception {
-		LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
-		// XXX Authenticate with HR role when making this request
-		ResponseEntity<LeaveRequestDTO> response = resttemplate.postForEntity("/deny/{id}", null,
-				LeaveRequestDTO.class, saved.getId());
-		assertThat(response.getStatusCode()).isEqualByComparingTo(ACCEPTED);
-		assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
-		assertThat(response.getBody().getEmployee()).isEqualTo("Alice");
-		assertThat(response.getBody().getStatus()).isEqualByComparingTo(DENIED);
-	}
+		@Test
+		void testApproveMissing() {
+			// XXX Authenticate with HR role when making this request
+			ResponseEntity<LeaveRequestDTO> response = restTemplate.postForEntity("/approve/{id}", null,
+					LeaveRequestDTO.class, UUID.randomUUID());
+			assertThat(response.getStatusCode()).isEqualByComparingTo(NO_CONTENT);
+		}
 
-	@Test
-	void testViewId() throws Exception {
-		LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
-		// XXX Authenticate as Alice when making this request
-		ResponseEntity<LeaveRequestDTO> response = resttemplate.getForEntity("/view/id/{id}",
-				LeaveRequestDTO.class, saved.getId());
-		assertThat(response.getStatusCode()).isEqualByComparingTo(OK);
-		assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
-		assertThat(response.getBody().getEmployee()).isEqualTo("Alice");
-		assertThat(response.getBody().getStatus()).isEqualByComparingTo(PENDING);
-	}
+		@Test
+		void testDeny() {
+			LeaveRequest saved = repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
+			// XXX Authenticate with HR role when making this request
+			ResponseEntity<LeaveRequestDTO> response = restTemplate.postForEntity("/deny/{id}", null,
+					LeaveRequestDTO.class, saved.getId());
+			assertThat(response.getStatusCode()).isEqualByComparingTo(ACCEPTED);
+			assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
+			assertThat(response.getBody().getEmployee()).isEqualTo("Alice");
+			assertThat(response.getBody().getStatus()).isEqualByComparingTo(DENIED);
+		}
 
-	@Test
-	void testViewIdMissing() throws Exception {
-		// XXX Authenticate with HR role when making this request
-		ResponseEntity<LeaveRequestDTO> response = resttemplate.getForEntity("/view/id/{id}", LeaveRequestDTO.class,
-				UUID.randomUUID());
-		assertThat(response.getStatusCode()).isEqualByComparingTo(NO_CONTENT);
-	}
+		@Test
+		void testViewIdMissing() {
+			// XXX Authenticate with HR role when making this request
+			ResponseEntity<LeaveRequestDTO> response = restTemplate.getForEntity("/view/id/{id}", LeaveRequestDTO.class,
+					UUID.randomUUID());
+			assertThat(response.getStatusCode()).isEqualByComparingTo(NO_CONTENT);
+		}
 
-	private static final ParameterizedTypeReference<List<LeaveRequestDTO>> typeref = new ParameterizedTypeReference<>() {
-	};
-
-	@Test
-	void testViewEmployee() throws Exception {
-		repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
-		// XXX Authenticate as Alice when making this request
-		ResponseEntity<List<LeaveRequestDTO>> response = resttemplate.exchange("/view/employee/{employee}", GET, null,
-				typeref, "Alice");
-		assertThat(response.getStatusCode()).isEqualByComparingTo(OK);
-		assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
-		assertThat(response.getBody().get(0).getEmployee()).isEqualTo("Alice");
-		assertThat(response.getBody().get(0).getStatus()).isEqualByComparingTo(PENDING);
-	}
-
-	@Test
-	void testViewAll() throws Exception {
-		repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
-		// XXX Authenticate with HR role when making this request
-		ResponseEntity<List<LeaveRequestDTO>> response = resttemplate.exchange("/view/all", GET, null, typeref);
-		assertThat(response.getStatusCode()).isEqualByComparingTo(OK);
-		assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
-		assertThat(response.getBody().get(0).getEmployee()).isEqualTo("Alice");
-		assertThat(response.getBody().get(0).getStatus()).isEqualByComparingTo(PENDING);
+		@Test
+		void testViewAll() {
+			repository.save(new LeaveRequest("Alice", of(2019, 11, 30), of(2019, 12, 3), PENDING));
+			// XXX Authenticate with HR role when making this request
+			ResponseEntity<List<LeaveRequestDTO>> response = restTemplate.exchange("/view/all", GET, null, TYPE_REFERENCE);
+			assertThat(response.getStatusCode()).isEqualByComparingTo(OK);
+			assertThat(response.getHeaders().getContentType()).isEqualByComparingTo(APPLICATION_JSON);
+			assertThat(response.getBody().get(0).getEmployee()).isEqualTo("Alice");
+			assertThat(response.getBody().get(0).getStatus()).isEqualByComparingTo(PENDING);
+		}
 	}
 
 }
