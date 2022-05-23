df3dabb2d3b96129c8db12f314ff5cae483b812d
Tom de Vroomen <social@tomdevroomen.nl>
2022-05-23 22:31:11 +0200

    Add steps to guide the workshop participant for audit-spring-data-entities (#81)
    Co-authored-by: Tom de Vroomen <tom.de.vroomen@jdriven.com>

---
+++ audit-spring-data-entities/src/main/java/com/jdriven/AuditSecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.jdriven;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditSecurityConfiguration {
+    // TODO Configure JPA to look up the Author corresponding to the active user and store that along with any BlogPost
+}
--- audit-spring-data-entities/src/main/java/com/jdriven/SecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package com.jdriven;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SecurityConfig {
-	// TODO Configure JPA to look up the Author corresponding to the active user and store that along with any BlogPost
-}
