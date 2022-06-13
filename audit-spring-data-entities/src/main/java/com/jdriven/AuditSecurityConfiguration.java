e77f0f14714cbf1c918fe55e866a0599dc090a02
Tim te Beek <tim.te.beek@jdriven.com>
2022-06-13 09:17:33 +0200

    Apply Spotless

--- audit-spring-data-entities/src/main/java/com/jdriven/AuditSecurityConfiguration.java
+++ audit-spring-data-entities/src/main/java/com/jdriven/AuditSecurityConfiguration.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Configuration
 @EnableJpaAuditing
 public class AuditSecurityConfiguration {
-    // TODO Configure JPA to look up the Author corresponding to the active user and store that along with any BlogPost
+	// TODO Configure JPA to look up the Author corresponding to the active user and store that along with any BlogPost
 }
